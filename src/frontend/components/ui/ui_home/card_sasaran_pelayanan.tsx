cc69a0c24c81fa690cf431cd41d3f63c131c64d5
radeet <greenglass0k@gmail.com>
2026-09-13 22:56:36 +0800

    committing before pulling from frontend

--- src/frontend/components/ui/ui_home/card_sasaran_pelayanan.tsx
+++ src/frontend/components/ui/ui_home/card_sasaran_pelayanan.tsx
@@ -9,7 +9,7 @@ interface CardSasaranProps {
 export function CardSasaranPelayanan({ title }: CardSasaranProps) {
     return (
         <Link href="/about#sasaran_layanan">
-        <div className=" lg:mx-4 cursor-pointer p-4 whitespace-pre-line h-full relative flex flex-col items-center justify-center tracking-wide bg-linear-to-b from-teal-400 to-teal-500 overflow-hidden rounded-2xl text-white min-h-[110px] shadow-lg/30 transition-all duration-300 ease-out hover:-translate-y-1.5 hover:shadow-[0_15px_25px_-5px_rgba(0,0,0,0.25),0_0_20px_theme(colors.teal.400/70%)]">
+        <div className=" lg:mx-4 cursor-pointer p-4 whitespace-pre-line h-full relative flex flex-col items-center justify-center tracking-wide bg-linear-to-b from-teal-400 to-teal-500 overflow-hidden rounded-2xl text-white min-h-[110px] shadow-md/30 transition-all duration-300 ease-out hover:-translate-y-1.5 hover:shadow-[0_15px_25px_-5px_rgba(0,0,0,0.25),0_0_20px_theme(colors.teal.400/30%)]">
             {/* ORNAMENT AREA */}
 
             <Image
---
+++ src/frontend/types/gallery.ts
@@ -0,0 +1,11 @@
+export interface Gallery {
+    id: number;
+    title: string;
+    description: string;
+    image_url: string;
+    file_id: null;
+    price: null;
+    stock: number;
+    status: 'active';
+    category: string;
+}
