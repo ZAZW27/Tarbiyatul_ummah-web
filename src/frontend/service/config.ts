3924d3182d0dae7e1f99ff2101a02bd80ea41396
ZAZW27 <clawsrage27@gmail.com>
2026-09-19 15:58:42 +0800

    this is bad2

--- src/frontend/service/config.ts
+++ src/frontend/service/config.ts
@@ -1,4 +1,4 @@
-export const API_BASE_URL = '/api';
+export const API_BASE_URL = process.env.BACKEND_URL;
 
 export const buildQueryParams = (page: number, limit: number, categories?: string[]) => {
     const params = new URLSearchParams({
